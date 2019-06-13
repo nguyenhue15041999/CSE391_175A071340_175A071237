6102652710675a1449ae28a5197d180f9c2ebd9c
= <=>
2019-06-13 19:47:51 +0700

    Loan da tai len tep

--- HanuUniversity/Admin/js/login.js
+++ HanuUniversity/Admin/js/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#login').click(function() {
         if ($('#name').val()=='HueLoan' && $('#pass').val()=='123456789') {
-            window.location.href='quantrihethong.html';
+            window.location.href='Quantrihethong.html';
         }else{
           $( ".notification" ).show();
       }
